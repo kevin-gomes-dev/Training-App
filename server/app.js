4e2ef482df10f5d72febf1fe230931da7963665e
Kevin Gomes <kevin.gomes.dev@gmail.com>
2026-09-15 16:11:56 -0400

    put and delete requests for users

--- server/app.js
+++ server/app.js
@@ -1,16 +1,18 @@
 import express from "express";
 import morgan from "morgan";
-import requireBody from "./middleware/requireBody.js";
 import usersRoute from "./api/usersRoute.js";
 import messagesRoute from "./api/messagesRoute.js";
 import getUserFromToken from "./middleware/getUserFromToken.js";
 import requireAdmin from "./middleware/requireAdmin.js";
 const app = express();
 export default app;
+
 const JSON_PARSE_ERROR_TYPE = "entity.parse.failed";
 const INVALID_TYPE_ERROR_CODE = "22P02";
 const UNIQUE_CONSTRAINT_ERROR_CODE = "23505";
 const FOREIGN_KEY_ERROR_CODE = "23503";
+const INDETERMINATE_DATA_TYPE_ERROR_CODE = "42P18";
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(getUserFromToken);
@@ -30,6 +32,8 @@ app.use((err, req, res, next) => {
     // Foreign key violation
     case FOREIGN_KEY_ERROR_CODE:
       return res.status(400).send(err.detail);
+    case INDETERMINATE_DATA_TYPE_ERROR_CODE:
+      return res.status(400).send(err.message);
     default:
       next(err);
   }
--- server/db/queries/users.js
+++ server/db/queries/users.js
@@ -1,7 +1,7 @@
 import db from "../client.js";
 import bcrypt from "bcrypt";
 
-// Store the hashed password in case of data breach
+/** Store the hashed password in case of data breach */
 export async function insertUser({ username, password, role }) {
   const SQL = `INSERT INTO users(username,password,role) VALUES($1,$2,$3) RETURNING *`;
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -11,7 +11,7 @@ export async function insertUser({ username, password, role }) {
   return user;
 }
 
-// Instead of comparing hashed password to db password, uses bcrypt compare
+/** Instead of comparing hashed password to db password, uses bcrypt compare method*/
 export async function getUserByUsername({ username, password }) {
   const SQL = `SELECT * FROM users WHERE username = $1`;
   const {
@@ -34,4 +34,27 @@ export async function getUserById({ id }) {
     rows: [user],
   } = await db.query(SQL, [id]);
   return user;
+}
+
+/** If no username, only updates password. */
+export async function updateUser({ id, username, password }) {
+  let SQL;
+  if (!username) SQL = `UPDATE users SET password = $3 WHERE id = $1`;
+  else
+    SQL = `UPDATE users SET username = $2, password = $3
+  WHERE id = $1 AND username IS DISTINCT FROM $2 RETURNING *`;
+  console.log(SQL);
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const {
+    rows: [user],
+  } = await db.query(SQL, [id, username, hashedPassword]);
+  return user;
+}
+
+export async function deleteUser({ id }) {
+  const SQL = `DELETE FROM users WHERE id = $1`;
+  const {
+    rows: [user],
+  } = await db.query(SQL, [id]);
+  return null;
 }
